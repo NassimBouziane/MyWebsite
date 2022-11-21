12a233b08563e6e399c6b87a7c6e3946ee74991c
Jordan <hoarau_j@etna-alternance.net>
2022-11-21 11:35:13 +0100

    adding feature account (profil/parameter)

--- my_website/pages/compte/moi.tsx
+++ my_website/pages/compte/moi.tsx
@@ -5,12 +5,15 @@ export default function moi(){
         <NavBar/>
         <div id='BoxInfo'>
             <div>
-                <div className='account_title'>
-                    <p >Compte</p>
-                </div>
-                <div className='account_title'>
-                    <p >Paramètre</p>
-                </div>
+                <button className='account_title'>Compte</button>
+            
+                <button className='account_title'>Paramètre</button>
+            </div>
+            <div className='account_info'>
+                <h1>je suis info</h1>
+            </div>
+            <div className='account_parameter'>
+                <h1>je suis parametre</h1>
             </div>
         </div>
         <Footer/>
