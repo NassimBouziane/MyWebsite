c4138dcac8f155e9ae2a38e7fd235127ecaa0132
Jordan <hoarau_j@etna-alternance.net>
2022-11-23 13:50:16 +0100

    hidden password done

--- my_website/pages/compte/moi.tsx
+++ my_website/pages/compte/moi.tsx
@@ -8,34 +8,55 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import { useEffect, useState, useEffec } from 'react';
 import { fetchUser } from '../../user/userService';
+import { isComputedPropertyName } from 'typescript';
 
 
+let Bool = false
 export default function moi(){
 const [value, setValue] = React.useState(0);
 const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
-};  
+};
 const [data, setData] = useState(null)
-  
 useEffect(() => {
- fetchUser(77).then((response) => {
+ fetchUser(11).then((response) => {
       setData(response.data)
 })
 }, [])
+const [showPassword,setShowPassword] = useState ("")
+let [censoredPassword, setCensoredPassword] = useState("")
+let hiddenPassword=""
+function compte(){
+if (data !== null){
+for(let i=0;data.password.length > i; i+=1){
+  hiddenPassword = hiddenPassword + "*"
+}
+setCensoredPassword(hiddenPassword)
+}}
 
+function handleclick(){
+  if (data !== null && Bool === false){
+    Bool = true
+    setCensoredPassword(data.password);
+  }
+  else if (Bool===true){
+    compte()
+    Bool = false;
+  }
+}
  return <div>
     <NavBar/>
     <Box id="BoxInfo">
       <TabContext value={value}>
         <Box >
           <TabList onChange={handleChange} aria-label="lab API tabs example"TabIndicatorProps={{style:{backgroundColor:"#eff8f6"}}} textColor="inherit"  >
-            <Tab disableRipple style={{minWidth:"50%"}} className='account_title' label="Compte" value="1" />
+            <Tab onClick={compte} disableRipple style={{minWidth:"50%"}} className='account_title' label="Compte" value="1" />
             <Tab disableRipple style={{minWidth:"50%"}} className='account_title' label="Paramètre" value="2" />
           </TabList>
         </Box>
-        <TabPanel  value="1">Nom d'utilisateur {data && <p>{data.username} </p>} Mot de passe {data && <p className='password'>{data.password}</p>}</TabPanel>
+        <TabPanel  value="1">Nom d'utilisateur {data && <p>{data.username} </p>} Mot de passe {data && <p className='password'>{censoredPassword}</p>} <button onClick={handleclick}>hide</button></TabPanel>
         <TabPanel  value="2"></TabPanel>
-        <button onClick="">hide</button>
+       
       </TabContext>
     </Box>
     <Footer/>
