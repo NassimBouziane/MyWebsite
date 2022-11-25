c47f8b8f0153ba84866ac1b3896aa44153bbeba3
Jordan <hoarau_j@etna-alternance.net>
2022-11-25 11:02:20 +0100

    Account CSS DONE

--- my_website/pages/compte/moi.tsx
+++ my_website/pages/compte/moi.tsx
@@ -1,4 +1,4 @@
-import Footer from '../footer'
+import Footer from '../footer';
 import NavBar from '../header'
 import * as React from 'react'
 import Box from '@mui/material/Box'
@@ -53,15 +53,30 @@ function handleclick(){
             <Tab disableRipple style={{minWidth:"50%"}} className='account_title' label="Paramètre" value="2" />
           </TabList>
         </Box>
-        <TabPanel  value="1"><span className='accountInfo'>Prenom</span> {data && <p>{data.firstName} </p>}
-         <span className='accountInfo'>Nom</span> {data && <p>{data.lastName}</p>}
-         <span className='accountInfo'>Nom d'utilisateur</span> {data && <p >{data.username} </p>}
-         <span className='accountInfo'>Email</span> {data && <p>{data.email} </p>}
-         <span className='accountInfo'>Mot de passe</span> {data && <p>{censoredPassword} <p><BsFillEyeSlashFill onClick={handleclick}/> </p></p>}
-         <span className='accountInfo'>Numero de téléphone</span> {data && <p >{data.phoneNumber} </p>} 
+        <TabPanel  value="1" className='panelInfo'><div className='divPanel'>
+          <span className='accountInfo'>Prenom</span> {data && <p>{data.firstName} </p>}
+           <span className='accountInfo'>Nom</span> {data && <p>{data.lastName}</p>}
+           <span className='accountInfo'>Nom d'utilisateur</span> {data && <p >{data.username} </p>}
+        </div>
+         <div className='divPanel'>
+           <span className='accountInfo'>Email</span> {data && <p>{data.email} </p>}
+           <span className='accountInfo'>Mot de passe</span> {data && <p>{censoredPassword}  <BsFillEyeSlashFill className='BsFillEyeSlashFill' onClick={handleclick}/>  </p>}
+           <span className='accountInfo'>Numero de téléphone</span> {data && <p >{data.phoneNumber} </p>}
+         </div>
           </TabPanel>
-        <TabPanel  value="2"></TabPanel>
+        <TabPanel  value="2">
+          <div>
+            <form className='formAccount'>
+              <label>Nom d'utilisateur <input className='inputAccount' id='formInput' type="text" value={data && data.username}/></label>
+              <label>Email <input className='inputAccount' id='formInput' type="email" value={data && data.email}/></label>
+              <label>Numero de telephone <input className='inputAccount' id='formInput' type="tel" value={data && data.phoneNumber}/></label>
+              <label>Mot de passe <input className='inputAccount' id='formInput' type="password" value={data && {censoredPassword}}/></label>
+              <input type="submit" value="SUBMIT"/>
+            </form>
+          </div>
+        </TabPanel>
       </TabContext>
     </Box>
+    {/* <Footer/> */}
 </div>
 }
