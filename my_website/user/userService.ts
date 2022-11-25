322483f0ef88677f0a9a29b3d34a41dfc778ba1b
nassimbouziane <bouzianenassim17102002@gmail.com>
2022-11-25 16:09:30 +0100

    update user

--- my_website/user/userService.ts
+++ my_website/user/userService.ts
@@ -3,14 +3,13 @@ import { api } from '../services/servicesHelper'
 import { Cookies, useCookies } from 'react-cookie'
 import { useState } from 'react'
 import { getCookie, setCookie } from 'typescript-cookie'
-
 const Data2 = {
   username: 'test12H00',
   password: 'passwordeazeatest'
 }
 const Data1 = {
-  username: 'TESTOazezazeazezaaeza',
-  password: 'passwordeazeatest'
+  username: 'joshu1ds',
+  password: 'jeuispasmdp7ds5.'
 }
 
 export const fetchUser = async (id: string | number) => {
@@ -44,5 +43,39 @@ export const deletebyId = async (id2: number) => {
     })
     .catch(function (error) {
       console.log(error)
+    })
+}
+export const modifyUsers = async (
+  firstName: any,
+  lastName: any,
+  username: any,
+  email: any,
+  phoneNumber: any,
+  password: any
+) => {
+  fetchUser(Data1.username).then((response) => {
+    setCookie('Id', response.data.id)
+  })
+
+  return await api
+    .put(
+      '/user',
+      {
+        id: getCookie('Id'),
+        firstName,
+        lastName,
+        username,
+        password,
+        email,
+        phoneNumber
+      },
+      {
+        headers: {
+          Authorization: getCookie('Authorization')
+        }
+      }
+    )
+    .then((response) => {
+      console.log('test2', response)
     })
 }
